01637954deeac01efaf8df36cbb031366c6884de
Your Name <youremail@example.com>
2023-09-12 00:09:37 +0500

    context api project

---
+++ contextAPI/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useContext(UserContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      {"  "}
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
